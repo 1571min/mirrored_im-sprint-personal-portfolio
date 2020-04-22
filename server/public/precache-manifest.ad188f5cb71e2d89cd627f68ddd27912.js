fa90e9b9cb3edad0b9dadc118b921fcd79598e30
lmt <1571min@naver.com>
2020-04-22 13:00:07 +0900

    init

---
+++ server/public/precache-manifest.ad188f5cb71e2d89cd627f68ddd27912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3081915dd051b77367b055fe7e46657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5913a0262874cd5eba8c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ac031e22626e650c6c3",
+    "url": "/static/js/2.bdca552c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdca552c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5913a0262874cd5eba8c",
+    "url": "/static/js/main.5b6e29e7.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
